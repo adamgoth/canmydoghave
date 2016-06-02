8ae988611501c41bc8d83a1e6ed259ba2ed0cf37
Adam Goth <adamgoth@gmail.com>
2016-06-02 16:36:59 -0600

    Adds new food data

--- src/misc/data.js
+++ src/misc/data.js
@@ -1,4 +1,10 @@
 module.exports = {
+  "almonds":
+    {
+      "answer": "No",
+      "info": "This one is tricky. While almonds are generally thought of as healthy, it is not recommended to feed them to your dog. Certain types of nuts can cause toxic poisonings, an upset stomach or an obstruction in your dog's gastrointestinal tract which can lead to life-saving surgery and unexpected veterinary expenses.",
+      "source": "http://www.petinsurance.com/"
+    },
   "apples":
     {
       "answer": "Yes",
@@ -10,6 +16,12 @@ module.exports = {
         "answer": "No",
         "info": "No matter how good you think the guacamole is, you shouldn't give it to your dog. Avocados contain a substance called persin. It's harmless for humans who aren't allergic. But large amounts might be toxic to dogs. If you happen to be growing avocados at home, keep your dog away from the plants. Persin is in the leaves, seed, and bark, as well as in the fruit.",
         "source": "http://pets.webmd.com/"
+      },
+    "bananas":
+      {
+        "answer": "Yes",
+        "info": "Dogs can eat bananas in moderation. Bananas are a good source of potassium, manganese, vitamin B6, vitamin C, fiber, biotin and copper. Bananas are low in sodium and cholesterol, but due to the sugar content in bananas, dogs should only eat them as a treat, and not as a regular part of their diets.",
+        "source": "http://www.pet360.com/"
       },
     "carrots":
       {
@@ -29,12 +41,24 @@ module.exports = {
         "info": "It's called dog food for a reason.",
         "source": "Common sense"
       },
+    "eggs":
+      {
+        "answer": "Yes",
+        "info": "Eggs are a great source of very digestible protein, riboflavin, and selenium. For some dogs that are prone to digestive upset, eggs can give them a little protein boost. Adding eggs to your dog's food is a healthy treat. Make sure to use cooked whole egg, as raw egg whites can cause biotin deficiency.",
+        "source": "http://www.moderndogmagazine.com/"
+      },
     "grapes":
       {
         "answer": "No",
         "info": "Grape and raisin (dried grapes) toxicity is well documented in dogs. Although the exact substance that causes the toxic reaction is not yet known, dogs should not eat grapes and raisins because even small amounts can prove to be fatally toxic for a dog.",
         "source": "http://www.petmd.com/"
       },
+    "popcorn":
+        {
+          "answer": "No",
+          "info": "While popcorn is not dangerous for your dog like chocolate or grapes are, it is not recommended to give your dog popcorn. Thus, popcorn will add calories to your dogs' daily allowance, so too much popcorn at one time can be bad. Furthermore, some kernels don't 'pop', and can cause tooth damage, become lodged in the mouth and cause infections, dental disease, or cause an upset stomach.",
+          "source": "http://company.justfoodfordogs.com/"
+        },
     "raisins":
         {
           "answer": "No",
@@ -46,5 +70,11 @@ module.exports = {
         "answer": "No",
         "info": "It's not a good idea to share salty foods like chips or pretzels with your dog. Eating too much salt can cause excessive thirst and urination and lead to sodium ion poisoning. Symptoms of too much salt include vomiting, diarrhea, depression, tremors, elevated body temperature, and seizures. It may even cause death.",
         "source": "http://pets.webmd.com/"
+      },
+    "yogurt":
+      {
+        "answer": "Yes",
+        "info": "Yogurt is a good source of available calcium and protein. When choosing yogurt, pick one that has live active bacteria and no sugars or artificial sweeteners. The active bacteria may act as probiotics. If your pooch is pudgy, make sure that you pick fat-free yogurt but not one that contains fat substitutes (e.g., Simplesse or Olestra). Frozen yogurt is a nice summer treat for dogs.",
+        "source": "http://www.moderndogmagazine.com/"
       },
 };
